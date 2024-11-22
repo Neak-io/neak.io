4352954602d90de371cbf8a30e5b31b7c054ffae
rinx-dev <rinxdev2023@gmail.com>
2024-11-22 09:29:15 +0700

    pages

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  rewrites: async () => {
-    return [
-      {
-        source: "/gtag/js",
-        destination: "https://www.googletagmanager.com/gtag/js",
-      },
-    ];
+  // for static site deployment
+  output: "export",
+  images: {
+    unoptimized: true,
   },
 };
 
